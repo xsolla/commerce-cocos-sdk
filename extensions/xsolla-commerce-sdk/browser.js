2040d4ed57e3541a9342e27f778c9ed3d8483fb6
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-10-25 15:11:30 +0300

    SDK-2114 Add Xsolla Commerce SDK extension

---
+++ extensions/xsolla-commerce-sdk/browser.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// The method defined in the extension
+exports.methods = {};
+
+// Execute when the extension is started
+exports.load = function() {};
+
+// Execute when the extension is closed
+exports.unload = function() {};
---
+++ extensions/xsolla-commerce-sdk/declarations/cc.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @deprecated Global variable `cc` was dropped since 3.0. Use ES6 module syntax to import Cocos Creator APIs.
+ */
+declare const cc: never;
+    
