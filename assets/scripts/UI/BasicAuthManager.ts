28debe50a3c279e49ba3c06159b382c689594038
lelka <salinaolga@gmail.com>
2021-11-15 20:15:17 +0300

    SDK-2121 Add DemoUser button functionality.

--- assets/scripts/UI/BasicAuthManager.ts
+++ assets/scripts/UI/BasicAuthManager.ts
@@ -26,6 +26,9 @@ export class BasicAuthManager extends Component {
     @property(Toggle)
     remeberMeToggle: Toggle;
 
+    @property(Button)
+    demoUserButton: Button;
+
     start() {
         
     }
@@ -63,6 +66,14 @@ export class BasicAuthManager extends Component {
         })
     }
 
+    onDemoUserClicked() {
+
+        this.usernameEditBox.string = 'xsolla';
+        this.passwordEditBox.string = 'xsolla';
+
+        this.onLoginClicked();
+    }
+
     onCredentialsChanged() {
         this.logInButton.interactable = this.usernameEditBox.string.length > 0 && this.passwordEditBox.string.length > 0;
     }
@@ -70,6 +81,7 @@ export class BasicAuthManager extends Component {
     addListeners () {
         this.backButton.node.on('click', this.onBackClicked, this);
         this.logInButton.node.on('click', this.onLoginClicked, this);
+        this.demoUserButton.node.on('click', this.onDemoUserClicked, this);
         this.usernameEditBox.node.on('text-changed', this.onCredentialsChanged, this);
         this.passwordEditBox.node.on('text-changed', this.onCredentialsChanged, this);
     }
@@ -77,6 +89,7 @@ export class BasicAuthManager extends Component {
     removeListeners () {
         this.backButton.node.off('click', this.onBackClicked, this);
         this.logInButton.node.off('click', this.onLoginClicked, this);
+        this.demoUserButton.node.off('click', this.onDemoUserClicked, this);
         this.usernameEditBox.node.off('text-changed', this.onCredentialsChanged, this);
         this.passwordEditBox.node.off('text-changed', this.onCredentialsChanged, this);
     }
