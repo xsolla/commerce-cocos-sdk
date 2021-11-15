994cb3d35e44788a9864b7970492f51b98edc412
lelka <salinaolga@gmail.com>
2021-11-15 23:52:14 +0300

    SDK-2121 Add shortPollingCheckOrder method.

--- assets/scripts/UI/MainMenuManager.ts
+++ assets/scripts/UI/MainMenuManager.ts
@@ -1,6 +1,8 @@
 // Copyright 2021 Xsolla Inc. All Rights Reserved.
 
 import { _decorator, Component, Node, Button } from 'cc';
+import { PaymentTokenResult, StoreItemsList, XsollaCommerce } from '../../../extensions/xsolla-commerce-sdk/assets/scripts/api/XsollaCommerce';
+import { XsollaLogin } from '../../../extensions/xsolla-commerce-sdk/assets/scripts/api/XsollaLogin';
 import { UIManager } from './UIManager';
 const { ccclass, property } = _decorator;
  
@@ -12,10 +14,6 @@ export class MainMenuManager extends Component {
 
     @property(Button)
     logOutButton: Button;
-
-    start() {
-        
-    }
 
     onDestroy() {
         this.removeListeners();
@@ -39,5 +37,22 @@ export class MainMenuManager extends Component {
 
     onLogoutClicked() {
         this.uiManager.openStartingScreen(this.node);
+    }
+
+    shortPollingCheckOrder(orderId: number) {
+        XsollaCommerce.checkOrder(XsollaLogin.cachedToken.access_token, XsollaCommerce.cachedPaymentResult.orderId, result => {
+            console.log('shortPollingCheckOrder ' + result.status);
+            if(result.status == 'done')
+            {
+                this.uiManager.openMessageScreen('success purchase!');
+                return;
+            }
+            if(result.status == 'new' || result.status == 'paid')
+            {
+                setTimeout( result => {
+                    this.shortPollingCheckOrder(orderId);
+                }, 3000);
+            }
+        })
     }
 }
