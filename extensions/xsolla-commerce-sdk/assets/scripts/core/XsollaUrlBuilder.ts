58fac561f20412a04ab9fa10f0582f6b2d8931ac
lelka <salinaolga@gmail.com>
2021-11-15 20:16:35 +0300

    SDK-2121 Add XsollaUrlBuilder.setPathParam method.

--- extensions/xsolla-commerce-sdk/assets/scripts/core/XsollaUrlBuilder.ts
+++ extensions/xsolla-commerce-sdk/assets/scripts/core/XsollaUrlBuilder.ts
@@ -3,18 +3,33 @@
 export class XsollaUrlBuilder {
     private _url: string;
     private _parameters = {};
+    private _pathParameters = {};
 
     constructor(url:string) {
         this._url = url;
     }
 
     build() : string {
+
+        for (var key in this._pathParameters) {
+            let paramPlaceholder = '{' + key + '}';
+            if(this._url.includes(paramPlaceholder))
+            {
+                this._url = this._url.replace(paramPlaceholder, this._pathParameters[key]);
+            }
+        }
+
         var queryParamsStr: string = '';
         for (var key in this._parameters) {
             queryParamsStr += queryParamsStr ? '&' : '?';
             queryParamsStr += key + '=' + encodeURIComponent(this._parameters[key]);
         }
         return this._url + queryParamsStr;
+    }
+
+    setPathParam(paramName:string, paramValue:string) : XsollaUrlBuilder {
+        this._pathParameters[paramName] = paramValue;
+        return this;
     }
 
     addStringParam(paramName:string, paramValue:string, ignoreEmpty:boolean = true) : XsollaUrlBuilder {
