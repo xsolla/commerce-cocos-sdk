75efc7ca6c45a759254870d404e7047ace452fee
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-11-18 17:29:33 +0200

    SDK-2118 Add demo for authentication by device ID on Android

--- assets/scripts/UI/StartingScreenManager.ts
+++ assets/scripts/UI/StartingScreenManager.ts
@@ -1,6 +1,7 @@
 // Copyright 2021 Xsolla Inc. All Rights Reserved.
 
-import { _decorator, Component, Node, Button } from 'cc';
+import { _decorator, Component, Node, Button, sys } from 'cc';
+import { XsollaLogin } from 'db://xsolla-commerce-sdk/scripts/api/XsollaLogin';
 import { UIManager } from './UIManager';
 const { ccclass, property } = _decorator;
  
@@ -14,10 +15,13 @@ export class StartingScreenManager extends Component {
     basicAuthButton: Button;
 
     @property(Button)
+    deviceIdAuthButton: Button;
+
+    @property(Button)
     otherAuthButton: Button;
 
     start() {
-        
+        this.deviceIdAuthButton.node.active = sys.isMobile;
     }
 
     onDestroy() {
@@ -34,16 +38,36 @@ export class StartingScreenManager extends Component {
 
     addListeners() {
         this.basicAuthButton.node.on('click', this.onBasicAuthClicked, this);
+        this.deviceIdAuthButton.node.on('click', this.onDeviceIdAuthClicked, this);
         this.otherAuthButton.node.on('click', this.onOtherAuthClicked, this);
     }
 
     removeListeners() {
         this.basicAuthButton.node.off('click', this.onBasicAuthClicked, this);
+        this.deviceIdAuthButton.node.off('click', this.onDeviceIdAuthClicked, this);
         this.otherAuthButton.node.off('click', this.onOtherAuthClicked, this);
     }
 
     onBasicAuthClicked() {
         this.uiManager.openBasicAuth(this.node);
+    }
+
+    onDeviceIdAuthClicked() {
+        let deviceId: string;
+        if(sys.platform.toLowerCase() == 'android') {
+            deviceId = jsb.reflection.callStaticMethod("com/cocos/game/XsollaNativeUtils", "getDeviceId", "()Ljava/lang/String;");
+        }
+        if(sys.platform.toLowerCase() == 'ios') {
+            deviceId = ''; // add native iOS implementation
+        }
+
+        XsollaLogin.authByDeviceId('Test', deviceId, 'xsollatest', 'xsollatest', token => {
+            console.log(token);
+            this.uiManager.openMainMenu(this.node);
+        }, err => {
+            console.log(err);
+            this.uiManager.openErrorScreen(this.node, err.description);
+        })
     }
 
     onOtherAuthClicked() {
