de1ce5abeafea549ec85caa6ec5a33737b9d64b9
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-11-18 22:33:59 +0200

    SDK-2118 Add native Android utility function for obtaining device name

--- native/engine/android/app/src/com/cocos/game/XsollaNativeUtils.java
+++ native/engine/android/app/src/com/cocos/game/XsollaNativeUtils.java
@@ -1,10 +1,15 @@
 package com.cocos.game;
 
+import android.os.Build;
 import android.provider.Settings;
 
 public class XsollaNativeUtils {
 
     public static String getDeviceId() {
         return Settings.Secure.getString(AppActivity.getAppActivity().getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    public static String getDeviceName() {
+        return Build.MANUFACTURER + Build.MODEL;
     }
 }
