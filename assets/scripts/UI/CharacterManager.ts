90d79461e1bb25c4dcd0e826129327fcba76151e
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-11-24 14:21:20 +0200

    SDK-2123 Add possibility to create new user attribute

--- assets/scripts/UI/CharacterManager.ts
+++ assets/scripts/UI/CharacterManager.ts
@@ -1,6 +1,6 @@
 // Copyright 2021 Xsolla Inc. All Rights Reserved.
 
-import { _decorator, Component, Node, Button, Prefab, ScrollView, instantiate } from 'cc';
+import { _decorator, Component, Node, Button, Prefab, ScrollView, instantiate, EditBox } from 'cc';
 import { UserAttribute, XsollaLogin } from '../../../extensions/xsolla-commerce-sdk/assets/scripts/api/XsollaLogin';
 import { TokenStorage } from '../Common/TokenStorage';
 import { AttributeItem } from './Misc/AttributeItem';
@@ -15,6 +15,27 @@ export class CharacterManager extends Component {
 
     @property(Button)
     backButton: Button;
+
+    @property(Button)
+    addAttributeButton: Button;
+
+    @property(Button)
+    saveButton: Button;
+
+    @property(Button)
+    cancelButton: Button;
+
+    @property(EditBox)
+    keyEditBox: EditBox;
+
+    @property(EditBox)
+    valueEditBox: EditBox;
+
+    @property(Node)
+    allAttributesScreen: Node;
+
+    @property(Node)
+    addAttributeScreen: Node;
 
     @property(Prefab)
     attributeItemPrefab: Prefab;
@@ -31,7 +52,36 @@ export class CharacterManager extends Component {
     }
 
     onEnable() {
-        this.attributesList.node.active = true;
+        this.openAllAttributesScreen(this.addAttributeScreen);
+        this.addListeners();
+    }
+
+    onDisable() {
+        this.removeListeners();
+    }
+
+    onBackClicked() {
+        this.uiManager.openMainMenu(this.node);
+    }
+
+    onAddAttributeClicked() {
+        this.openAddAttributeScreen(this.allAttributesScreen);
+    }
+
+    onSaveClicked() {
+        this.addAttribute(this.keyEditBox.string, this.valueEditBox.string);
+    }
+
+    onCancelClicked() {
+        this.openAllAttributesScreen(this.addAttributeScreen);
+    }
+
+    onAttributeDataChanged() {
+        this.saveButton.interactable = this.keyEditBox.string.length > 0 && this.valueEditBox.string.length > 0;
+    }
+
+    openAllAttributesScreen(currentScreen:Node) {
+        this.clearAttributesList();
 
         XsollaLogin.getUserAttributes(TokenStorage.token.access_token, null, null, attributes => {
             this.populateAttributesList(attributes);
@@ -39,26 +89,20 @@ export class CharacterManager extends Component {
         }, err => {
             console.log(err);
             this.uiManager.openErrorScreen(this.node, err.description);
-        })
-
-        this.addListeners();
-    }
-
-    removeAttribute(key:string, attributeItem:Node) {
-        XsollaLogin.removeUserAttributes(TokenStorage.token.access_token, [key], () => {
-            this.attributesList.content.removeChild(attributeItem);
-        }, err => {
-            console.log(err);
         });
+
+        currentScreen.active = false;
+        this.allAttributesScreen.active = true;
     }
 
-    onDisable() {
-        this.clearAttributesList();
-        this.removeListeners();
-    }
+    openAddAttributeScreen(currentScreen:Node) {
+        this.keyEditBox.string = '';
+        this.valueEditBox.string = '';        
 
-    onBackClicked() {
-        this.uiManager.openMainMenu(this.node);
+        currentScreen.active = false;
+        this.addAttributeScreen.active = true;
+
+        this.saveButton.interactable = false;
     }
 
     populateAttributesList(attributes: Array<UserAttribute>) {
@@ -70,15 +114,51 @@ export class CharacterManager extends Component {
         }
     }
 
+    addAttribute(key:string, value:string) {
+        let newAttrinbute: UserAttribute = {
+            key: key,
+            permission: 'public',
+            value: value
+        };
+
+        XsollaLogin.updateUserAttributes(TokenStorage.token.access_token, [newAttrinbute], () => {
+            this.openAllAttributesScreen(this.addAttributeScreen);
+        }, err => {
+            console.log(err);
+            this.uiManager.openErrorScreen(this.node, err.description);
+        });
+    }
+
+    removeAttribute(key:string, attributeItem:Node) {
+        XsollaLogin.removeUserAttributes(TokenStorage.token.access_token, [key], () => {
+            this.attributesList.content.removeChild(attributeItem);
+        }, err => {
+            console.log(err);
+            this.uiManager.openErrorScreen(this.node, err.description);
+        });
+    }
+
     clearAttributesList() {
         this.attributesList.content.destroyAllChildren();
     }
 
     addListeners () {
         this.backButton.node.on(Button.EventType.CLICK, this.onBackClicked, this);
+        this.addAttributeButton.node.on(Button.EventType.CLICK, this.onAddAttributeClicked, this);
+        this.saveButton.node.on(Button.EventType.CLICK, this.onSaveClicked, this);
+        this.cancelButton.node.on(Button.EventType.CLICK, this.onCancelClicked, this);
+
+        this.keyEditBox.node.on(EditBox.EventType.TEXT_CHANGED, this.onAttributeDataChanged, this);
+        this.valueEditBox.node.on(EditBox.EventType.TEXT_CHANGED, this.onAttributeDataChanged, this);
     }
 
     removeListeners () {
         this.backButton.node.off(Button.EventType.CLICK, this.onBackClicked, this);
+        this.addAttributeButton.node.off(Button.EventType.CLICK, this.onAddAttributeClicked, this);
+        this.saveButton.node.off(Button.EventType.CLICK, this.onSaveClicked, this);
+        this.cancelButton.node.off(Button.EventType.CLICK, this.onCancelClicked, this);
+
+        this.keyEditBox.node.off(EditBox.EventType.TEXT_CHANGED, this.onAttributeDataChanged, this);
+        this.valueEditBox.node.off(EditBox.EventType.TEXT_CHANGED, this.onAttributeDataChanged, this);
     }
 }
