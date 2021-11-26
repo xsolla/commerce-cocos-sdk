fd672d8374b2f9db6660e56b5ecf371080abfd0f
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-11-26 13:17:36 +0200

    SDK-2160 Add native iOS Xsolla SDK frameworks

--- native/engine/ios/XsollaNativeUtils.mm
+++ native/engine/ios/XsollaNativeUtils.mm
@@ -2,7 +2,7 @@
 #import <UIKit/UIKit.h>
 #import <AuthenticationServices/AuthenticationServices.h>
 
-//#import "XsollaSDKLoginKitUnity/XsollaSDKLoginKitUnity-Swift.h"
+#import "XsollaSDKLoginKitUnity/XsollaSDKLoginKitUnity-Swift.h"
 
 @interface XsollaNativeUtils: NSObject
 
@@ -19,38 +19,39 @@
 }
 
 +(void) authViaSocialNetwork:(NSString*)platform client:(NSNumber*)clientId state:(NSString*)stateStr redirect:(NSString*)redirectUriStr {
-//	OAuth2Params *oauthParams = [[OAuth2Params alloc] initWithClientId:clientId
-//																 state:stateString
-//																 scope:@"offline"
-//																 redirectUri:redirectUriString];
-//
-//	JWTGenerationParams *jwtGenerationParams = [[JWTGenerationParams alloc] initWithGrantType:TokenGrantTypeAuthorizationCode
-//														 clientId:clientId
-//														 refreshToken:nil
-//														 clientSecret:nil
-//														 redirectUri:redirectUriString];
-//
-//	if (@available(iOS 13.4, *)) {
-//		UIWindow* window = [[UIApplication sharedApplication] keyWindow];
-//		WebAuthenticationPresentationContextProvider* context = [[WebAuthenticationPresentationContextProvider alloc] initWithPresentationAnchor:window];
-//
-//		[[LoginKitUnity shared] authBySocialNetwork:platform oAuth2Params:oauthParams jwtParams:jwtGenerationParams presentationContextProvider:context completion:^(AccessTokenInfo * _Nullable accesTokenInfo, NSError * _Nullable error){
-//
-//			if(error != nil) {
-//				NSLog(@"Error code: %ld", error.code);
-//
-//				if(error.code == NSError.loginKitErrorCodeASCanceledLogin) {
-//					return;
-//				}
-//
-//				return;
-//			}
-//
-//			NSString* tokenInfoString = [XsollaUtils serializeTokenInfo:accesTokenInfo];
-//		}];
-//	} else {
-//		NSLog(@"Authentication via social networks with Xsolla is not supported for current iOS version.");
-//	}
+	NSLog(@"boi");
+	OAuth2Params *oauthParams = [[OAuth2Params alloc] initWithClientId:[clientId integerValue]
+																 state:stateStr
+																 scope:@"offline"
+																 redirectUri:redirectUriStr];
+
+	JWTGenerationParams *jwtGenerationParams = [[JWTGenerationParams alloc] initWithGrantType:TokenGrantTypeAuthorizationCode
+														 clientId:[clientId integerValue]
+														 refreshToken:nil
+														 clientSecret:nil
+														 redirectUri:redirectUriStr];
+
+	if (@available(iOS 13.4, *)) {
+		UIWindow* window = [[UIApplication sharedApplication] keyWindow];
+		WebAuthenticationPresentationContextProvider* context = [[WebAuthenticationPresentationContextProvider alloc] initWithPresentationAnchor:window];
+
+		[[LoginKitUnity shared] authBySocialNetwork:platform oAuth2Params:oauthParams jwtParams:jwtGenerationParams presentationContextProvider:context completion:^(AccessTokenInfo * _Nullable accesTokenInfo, NSError * _Nullable error){
+
+			if(error != nil) {
+				NSLog(@"Error code: %ld", error.code);
+
+				if(error.code == NSError.loginKitErrorCodeASCanceledLogin) {
+					return;
+				}
+
+				return;
+			}
+
+			//NSString* tokenInfoString = [XsollaUtils serializeTokenInfo:accesTokenInfo];
+		}];
+	} else {
+		NSLog(@"Authentication via social networks with Xsolla is not supported for current iOS version.");
+	}
 }
 
 @end
