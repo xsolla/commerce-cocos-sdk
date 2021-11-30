d5e6490d9aa929ab17c77d636cea0d60545fcea1
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-11-30 15:15:01 +0200

    SDK-2159 Add native Android libraries for authentication via social networks

--- assets/scripts/UI/SocialAuthManager.ts
+++ assets/scripts/UI/SocialAuthManager.ts
@@ -1,8 +1,8 @@
 // Copyright 2021 Xsolla Inc. All Rights Reserved.
 
-import { _decorator, Component, Node, Button, EditBox, EventHandler, sys, System, Toggle, ScrollView, Prefab, SpriteFrame, instantiate } from 'cc';
+import { _decorator, Component, Node, Button, EditBox, EventHandler, sys, System, Toggle, ScrollView, Prefab, SpriteFrame, instantiate, CCString } from 'cc';
 import { XsollaLogin } from 'db://xsolla-commerce-sdk/scripts/api/XsollaLogin';
-import { Xsolla } from 'db://xsolla-commerce-sdk/scripts/Xsolla';
+import { Xsolla, XsollaAuthenticationType } from 'db://xsolla-commerce-sdk/scripts/Xsolla';
 import { Token } from 'db://xsolla-commerce-sdk/scripts/api/XsollaLogin';
 import { TokenStorage } from '../Common/TokenStorage';
 import { SocialNetworkItem } from './Misc/SocialNetworkItem';
@@ -12,7 +12,7 @@ const { ccclass, property } = _decorator;
 @ccclass('SocialNetworkItemData')
 export class SocialNetworkItemData {
 
-    @property(String)
+    @property(CCString)
     name: String = '';
 
     @property(SpriteFrame)
@@ -40,8 +40,35 @@ export class SocialAuthManager extends Component {
     @property([SocialNetworkItemData])
     socialNetworksData: SocialNetworkItemData[] = [];
 
+    @property(CCString)
+    facebookAppId: String;
+
+    @property(CCString)
+    googleAppId: String;
+
+    @property(CCString)
+    wechatAppId: String;
+
+    @property(CCString)
+    qqAppId: String;
+
     start() {
         this.populateSocialNetworksList();
+
+        if(sys.platform.toLowerCase() == 'android') {
+            if(Xsolla.settings.authType == XsollaAuthenticationType.Oauth2) {
+                jsb.reflection.callStaticMethod("com/cocos/game/XsollaNativeAuth", "xLoginInitOauth",
+                    "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V",
+                    Xsolla.settings.loginId, Xsolla.settings.clientId.toString(), "https://login.xsolla.com/api/blank",
+                    this.facebookAppId, this.googleAppId, this.wechatAppId, this.qqAppId);
+            }
+            if(Xsolla.settings.authType == XsollaAuthenticationType.Jwt) {
+                jsb.reflection.callStaticMethod("com/cocos/game/XsollaNativeAuth", "xLoginInitJwt",
+                    "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V",
+                    Xsolla.settings.loginId, "https://login.xsolla.com/api/blank",
+                    this.facebookAppId, this.googleAppId, this.wechatAppId, this.qqAppId);
+            }
+        }
     }
 
     onDestroy() {
@@ -88,6 +115,9 @@ export class SocialAuthManager extends Component {
         if(sys.platform.toLowerCase() == 'ios') {
             jsb.reflection.callStaticMethod("XsollaNativeUtils", "authViaSocialNetwork:client:state:redirect:",
                 socialNetworkName, Xsolla.settings.clientId, 'xsollatest', 'app://xsollalogin');
+        }
+        if(sys.platform.toLowerCase() == 'android') {
+            jsb.reflection.callStaticMethod("com/cocos/game/XsollaNativeAuth", "authSocial", "(Ljava/lang/String;ZZ)V", socialNetworkName, true, false);
         }
     }
 
---
+++ native/engine/android/app/src/com/cocos/game/XsollaNativeAuth.java
@@ -0,0 +1,48 @@
+// Copyright 2021 Xsolla Inc. All Rights Reserved.
+
+package com.cocos.game;
+
+import android.content.Intent;
+
+import androidx.annotation.Keep;
+
+import com.xsolla.android.login.XLogin;
+import com.xsolla.android.login.LoginConfig;
+import com.xsolla.android.login.social.SocialNetwork;
+
+@Keep
+public class XsollaNativeAuth {
+
+    @Keep
+    public static void xLoginInitJwt(String loginID, String callbackUrl, String facebookAppId, String googleAppId, String wechatAppId, String qqAppId) {
+        XLogin.SocialConfig socialConfig = new XLogin.SocialConfig(facebookAppId, googleAppId, wechatAppId, qqAppId);
+        LoginConfig loginConfig = new LoginConfig.JwtBuilder().
+                setProjectId(loginID).
+                setCallbackUrl(callbackUrl).
+                setSocialConfig(socialConfig).
+                build();
+        XLogin.init(AppActivity.getAppActivity(), loginConfig);
+    }
+
+    @Keep
+    public static void xLoginInitOauth(String loginID, String clientId, String callbackUrl, String facebookAppId, String googleAppId, String wechatAppId, String qqAppId) {
+        XLogin.SocialConfig socialConfig = new XLogin.SocialConfig(facebookAppId, googleAppId, wechatAppId, qqAppId);
+        LoginConfig loginConfig = new LoginConfig.OauthBuilder().
+                setProjectId(loginID).
+                setOauthClientId(Integer.parseInt(clientId)).
+                setCallbackUrl(callbackUrl).
+                setSocialConfig(socialConfig).
+                build();
+        XLogin.init(AppActivity.getAppActivity(), loginConfig);
+    }
+
+    @Keep
+    public static void authSocial(String provider, boolean rememberMe, boolean invalidateToken) {
+        SocialNetwork socialNetwork = SocialNetwork.valueOf(provider.toUpperCase());
+        Intent intent = new Intent(AppActivity.getAppActivity(), XsollaNativeAuthActivity.class);
+        intent.putExtra(XsollaNativeAuthActivity.ARG_SOCIAL_NETWORK, socialNetwork.name());
+        intent.putExtra(XsollaNativeAuthActivity.ARG_WITH_LOGOUT, invalidateToken);
+        intent.putExtra(XsollaNativeAuthActivity.REMEMBER_ME, rememberMe);
+        AppActivity.getAppActivity().startActivity(intent);
+    }
+}
