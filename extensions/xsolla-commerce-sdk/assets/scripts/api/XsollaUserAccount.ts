590b25cead8eb614bd3238ebf4fc99138a55b840
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-12-06 16:40:47 +0200

    SDK-2124 Add user acount data models

---
+++ extensions/xsolla-commerce-sdk/assets/scripts/api/XsollaUserAccount.ts
@@ -0,0 +1,54 @@
+// Copyright 2021 Xsolla Inc. All Rights Reserved.
+
+import { LoginError, XsollaError } from "../core/XsollaError";
+import { XsollaHttpUtil, XsollaRequestContentType } from "../core/XsollaHttpUtil";
+import { XsollaUrlBuilder } from "../core/XsollaUrlBuilder";
+
+export class XsollaUserAccount {
+    static getUserDetails(token:string, onComplete?:(userDetails:UserDetails) => void, onError?:(error:LoginError) => void) {
+        let url = new XsollaUrlBuilder('https://login.xsolla.com/api/users/me').build();
+
+        let request = XsollaHttpUtil.createRequest(url, 'GET', XsollaRequestContentType.None, token, result => {
+            let user = JSON.parse(result);
+            onComplete?.(user);
+        }, XsollaError.handleLoginError(onError));
+        request.send();
+    }
+}
+
+export interface UserBan {
+    date_from: string,
+    date_to: string,
+    reason: string
+}
+
+export interface UserGroup {
+    id: number,
+    is_default: boolean,
+    is_deletable: boolean,
+    name: string
+}
+
+export interface UserDetails {
+    ban: UserBan,
+    birthday: string,
+    connection_information: string,
+    country: string,
+    email: string,
+    external_id: string,
+    first_name: string,
+    gender: string,
+    groups: Array<UserGroup>,
+    id: string,
+    is_anonymous: boolean,
+    last_login: string,
+    last_name: string,
+    name: string,
+    nickname: string,
+    phone: string,
+    phone_auth: string,
+    picture: string,
+    registered: string,
+    tag: string,
+    username: string
+}
