dfea243c1a57d38788056738df827faf37c33e81
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-12-08 16:58:55 +0200

    SDK-2124 Add user account demo screen logic

---
+++ assets/scripts/UI/Misc/UserAccountItemComponent.ts
@@ -0,0 +1,70 @@
+// Copyright 2021 Xsolla Inc. All Rights Reserved.
+
+import { _decorator, Component, Node, Button, EditBox, Label } from 'cc';
+const { ccclass, property } = _decorator;
+ 
+@ccclass('UserAccountItemComponent')
+export class UserAccountItemComponent extends Component {
+    
+    @property(Button)
+    editButton: Button;
+
+    @property(Label)
+    titleLabel: Label;
+
+    @property(Label)
+    valueLabel: Label;
+
+    @property(EditBox)
+    valueEditBox: EditBox;
+
+    static ITEM_EDIT: string = 'itemEdit';
+
+    start() {
+
+    }
+
+    onEnable() {
+        this.addListeners();
+    }
+
+    onDisable() {
+        this.removeListeners();
+    }
+
+    onEditClicked() {
+        this.editButton.node.active = false;
+        this.titleLabel.node.active = false;
+        this.valueLabel.node.active = false;
+        this.valueEditBox.node.active = true;
+        this.valueEditBox.setFocus();
+    }
+
+    onValueEdited() {
+        if (this.valueEditBox.string != this.valueLabel.string) {
+            this.node.emit(UserAccountItemComponent.ITEM_EDIT, this.valueEditBox.string);
+        }
+
+        this.editButton.node.active = true;
+        this.titleLabel.node.active = true;
+        this.valueLabel.node.active = true;
+        this.valueEditBox.node.active = false;
+    }
+
+    setValue(value: string) {
+        this.valueLabel.string = value;
+        if (this.valueEditBox != null) {
+            this.valueEditBox.string = value;
+        }
+    }
+
+    addListeners () {
+        this.editButton?.node.on(Button.EventType.CLICK, this.onEditClicked, this);
+        this.valueEditBox?.node.on(EditBox.EventType.EDITING_DID_ENDED, this.onValueEdited, this);
+    }
+
+    removeListeners () {
+        this.editButton?.node.off(Button.EventType.CLICK, this.onEditClicked, this);    
+        this.valueEditBox?.node.off(EditBox.EventType.EDITING_DID_ENDED, this.onValueEdited, this);
+    }
+}
---
+++ assets/scripts/UI/UserAccountManager.ts
@@ -0,0 +1,128 @@
+// Copyright 2021 Xsolla Inc. All Rights Reserved.
+
+import { _decorator, Component, Node, Button, Label } from 'cc';
+import { UserDetails, UserDetailsUpdate, XsollaUserAccount } from '../../../extensions/xsolla-commerce-sdk/assets/scripts/api/XsollaUserAccount';
+import { TokenStorage } from '../Common/TokenStorage';
+import { UserAccountItemComponent } from './Misc/UserAccountItemComponent';
+import { UIManager } from './UIManager';
+const { ccclass, property } = _decorator;
+ 
+@ccclass('UserAccountManager')
+export class UserAccountManager extends Component {
+
+    @property(UIManager)
+    uiManager: UIManager;
+
+    @property(Button)
+    backButton: Button;
+
+    @property(UserAccountItemComponent)
+    emailItem: UserAccountItemComponent;
+
+    @property(UserAccountItemComponent)
+    usernameItem: UserAccountItemComponent;
+
+    @property(UserAccountItemComponent)
+    nicknameItem: UserAccountItemComponent;
+
+    @property(UserAccountItemComponent)
+    firstNameItem: UserAccountItemComponent;
+
+    @property(UserAccountItemComponent)
+    lastNameItem: UserAccountItemComponent;
+
+    @property(UserAccountItemComponent)
+    phoneNumberItem: UserAccountItemComponent;
+
+    start() {
+
+    }
+
+    onEnable() {
+        this.refreshUserAccountScreen();
+        this.addListeners();
+    }
+
+    onDisable() {
+        this.removeListeners();
+    }
+
+    onBackClicked() {
+        this.uiManager.openMainMenu(this.node);
+    }
+
+    refreshUserAccountScreen() {
+        XsollaUserAccount.getUserDetails(TokenStorage.token.access_token, userDetails => {
+            this.fillUserAccountItems(userDetails);
+        }, err => {
+            console.log(err);
+            this.uiManager.openErrorScreen(err.description);
+        });
+    }
+
+    updateUserAccountData(userDetailsUpdate: UserDetailsUpdate) {
+        XsollaUserAccount.updateUserDetails(TokenStorage.token.access_token, userDetailsUpdate, userDetails => {
+            this.fillUserAccountItems(userDetails);
+        }, err => {
+            console.log(err);
+            this.uiManager.openErrorScreen(err.description);
+            this.refreshUserAccountScreen();
+        });
+    }
+
+    fillUserAccountItems(userDetails: UserDetails) {
+        this.emailItem.setValue(userDetails.email);
+        this.usernameItem.setValue(userDetails.username);
+        this.nicknameItem.setValue(userDetails.nickname);
+        this.firstNameItem.setValue(userDetails.first_name);
+        this.lastNameItem.setValue(userDetails.last_name);
+        this.phoneNumberItem.setValue(userDetails.phone);
+    }
+
+    onNicknameEdited(value: string) {
+        let userDetailsUpdate: UserDetailsUpdate = {
+            nickname: value
+        }
+        this.updateUserAccountData(userDetailsUpdate);
+    }
+
+    onFirstNameEdited(value: string) {
+        let userDetailsUpdate: UserDetailsUpdate = {
+            first_name: value
+        }
+        this.updateUserAccountData(userDetailsUpdate);
+    }
+
+    onLastNameEdited(value: string) {
+        let userDetailsUpdate: UserDetailsUpdate = {
+            last_name: value
+        }
+        this.updateUserAccountData(userDetailsUpdate);
+    }
+
+    onPhoneNumberEdited(value: string) {
+        XsollaUserAccount.updateUserPhoneNumber(TokenStorage.token.access_token, value, () => {
+            this.phoneNumberItem.setValue(value);
+        }, err => {
+            console.log(err);
+            this.uiManager.openErrorScreen(err.description);
+            this.refreshUserAccountScreen();
+        });
+    }
+
+    addListeners () {
+        this.backButton.node.on(Button.EventType.CLICK, this.onBackClicked, this);
+        this.nicknameItem.node.on(UserAccountItemComponent.ITEM_EDIT, this.onNicknameEdited, this);
+        this.firstNameItem.node.on(UserAccountItemComponent.ITEM_EDIT, this.onFirstNameEdited, this);
+        this.lastNameItem.node.on(UserAccountItemComponent.ITEM_EDIT, this.onLastNameEdited, this);
+        this.phoneNumberItem.node.on(UserAccountItemComponent.ITEM_EDIT, this.onPhoneNumberEdited, this);
+    }
+
+    removeListeners () {
+        this.backButton.node.off(Button.EventType.CLICK, this.onBackClicked, this);
+        this.nicknameItem.node.off(UserAccountItemComponent.ITEM_EDIT, this.onNicknameEdited, this);
+        this.firstNameItem.node.off(UserAccountItemComponent.ITEM_EDIT, this.onFirstNameEdited, this);
+        this.lastNameItem.node.off(UserAccountItemComponent.ITEM_EDIT, this.onLastNameEdited, this);
+        this.phoneNumberItem.node.off(UserAccountItemComponent.ITEM_EDIT, this.onPhoneNumberEdited, this);
+    }
+}
