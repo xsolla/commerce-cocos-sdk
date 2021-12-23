80ad9be8c51ad78f67befe6033b340bfec8d7e95
Ivan Tustanivskyi <tustanivsky@gmail.com>
2021-12-23 15:44:52 +0200

    SDK-2192 Add utility for parsing JWT

---
+++ assets/scripts/Common/TokenParser.ts
@@ -0,0 +1,24 @@
+// Copyright 2021 Xsolla Inc. All Rights Reserved.
+
+import { Token } from 'db://xsolla-commerce-sdk/scripts/api/XsollaLogin';
+ 
+export class TokenUtils {
+    static getTokenParameter(token: Token, parameterName: string): any {
+        let tokenPayload = TokenUtils.parseTokenPayload(token);
+        if(tokenPayload.hasOwnProperty(parameterName)) {
+            return tokenPayload[parameterName];
+        }
+        return null;
+    }
+
+    static isTokenExpired(token: Token) {
+        let expirationTime = TokenUtils.getTokenParameter(token, 'exp') * 1000;
+        let currentTime = new Date().getTime();
+        return currentTime > expirationTime;
+    }
+
+    static parseTokenPayload(token: Token): any {
+        let payload = token.access_token.split('.')[1];
+        return JSON.parse(atob(payload));
+    }
+}
