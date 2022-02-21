78cf0d9fb7c054deafda2c886f619c9e4cead1da
Olga Salina <o.salina@xsolla.com>
2022-02-21 23:49:17 +0300

    SDK-2478 Update Main and Secondary button template

--- assets/scripts/UIKit/Buttons/ButtonController.ts
+++ assets/scripts/UIKit/Buttons/ButtonController.ts
@@ -1,13 +1,27 @@
 // Copyright 2021 Xsolla Inc. All Rights Reserved.
 
-import { _decorator, Component, Node, Label, Button, Color, color } from 'cc';
-const { ccclass, property } = _decorator;
- 
+import { _decorator, Component, Node, Label, Button, Color, color, Sprite } from 'cc';
+const { ccclass, property, executeInEditMode } = _decorator;
+
 @ccclass('ButtonController')
+@executeInEditMode
 export class ButtonController extends Component {
 
     @property(Label)
     ButtonText: Label;
+
+    @property({
+        displayName: 'Show Icon'
+    })
+    showIcon: boolean = false;
+
+    @property({
+        type:Sprite,
+        visible: function(): boolean {
+            return this.showIcon;
+        }
+    })
+    ButtonIcon: Sprite;
 
     @property(Color)
     NormalTextColor: Color = new Color(255, 255, 255, 200);
@@ -15,12 +29,32 @@ export class ButtonController extends Component {
     @property(Color)
     HoverTextColor: Color = new Color(255, 255, 255, 255);
 
+    update () {
+        if(this.ButtonIcon != null && this.ButtonIcon.node != null)
+        {
+            this.ButtonIcon.node.active = this.showIcon;
+        }
+    }
+
     start () {
+        this.ButtonText.color = this.NormalTextColor;
+        if(this.ButtonIcon != null)
+        {
+            this.ButtonIcon.color = this.NormalTextColor;
+        }
         this.node.on(Node.EventType.MOUSE_ENTER, () => {
             this.ButtonText.color = this.HoverTextColor;
+            if(this.ButtonIcon != null)
+            {
+                this.ButtonIcon.color= this.HoverTextColor;
+            }
         });
         this.node.on(Node.EventType.MOUSE_LEAVE, () => {
             this.ButtonText.color = this.NormalTextColor;
+            if(this.ButtonIcon != null)
+            {
+                this.ButtonIcon.color = this.NormalTextColor;
+            }
         });
     }
 }
