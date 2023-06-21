9795a71fb6fe9236bd91085db75e0bf5e52ff8c4
Olga Salina <o.salina@xsolla.com>
2023-06-21 15:56:43 +0300

    SDK-3180 Move checkPendingOrder to extension

--- extensions/xsolla-commerce-sdk/assets/scripts/common/OrderTracker.ts
+++ extensions/xsolla-commerce-sdk/assets/scripts/common/OrderTracker.ts
@@ -5,10 +5,25 @@ import { CommerceError } from "../core/Error";
 
 export class OrderTracker {
    
+    private static _cachedOrderCheckObjects: Array<OrderCheckObject> = [];
+
     static createOrderCheckObject(accessToken: string, orderId:number, onSuccess:() => void, onError:(error:CommerceError) => void) {
         let orderCheckObject = new OrderCheckObject();
         orderCheckObject.init(accessToken, orderId, onSuccess, onError);
         return orderCheckObject;
+    }
+
+    static checkPendingOrder(accessToken: string, orderId: number, onSuccess: () => void, onError: (error: CommerceError) => void) {
+        let orderCheckObject = OrderTracker.createOrderCheckObject(accessToken, orderId, () => {
+            onSuccess();
+            this._cachedOrderCheckObjects = this._cachedOrderCheckObjects.filter(obj => obj !== orderCheckObject);
+            orderCheckObject.destroy();
+        }, error => {
+            onError(error);
+            this._cachedOrderCheckObjects = this._cachedOrderCheckObjects.filter(obj => obj !== orderCheckObject);
+            orderCheckObject.destroy();
+        });
+        this._cachedOrderCheckObjects.push(orderCheckObject);
     }
 }
 
